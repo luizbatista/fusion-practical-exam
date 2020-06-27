0bb01882f38a8a17fb2a73b885da52a77a103cac
Luiz Batista <luiz.cavalcanti.batista@gmail.com>
2020-06-26 21:35:03 -0300

    first commit

--- README.md
+++ README.md
@@ -1,68 +1,39 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+![Fusion DMS](https://media-exp1.licdn.com/dms/image/C4E1BAQGn5EhkCP0Dog/company-background_10000/0?e=2159024400&v=beta&t=46Dr6QFBXOJprrRiBkem43ybdD5ml25vkqcZDZKmNs4)
 
-## Available Scripts
+# Teste Engenharia
 
-In the project directory, you can run:
+### `Instruções`
 
-### `yarn start`
+Estamos abrindo uma transportadora e queremos inicialmente listar, incluir, editar e deletar todos os nossos
+motoristas onde eles terão nome,cpf,email,situação (Livre, Em Curso, Retornando) e status (Ativo ou Não).
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+### `Preparando o Projeto`
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+No seu terminal, navegue até a sua pasta de preferência e rode o comando abaixo, isso irá baixar todos os arquivos para a sua máquina.
+> Lembre-se que você precisa do Git instalado em sua máquina, caso ainda não tenha: https://git-scm.com/
 
-### `yarn test`
+```shell
+git clone git@github.com:luizbatista/fusion-practical-exam.git
+```
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+### `Instalação`
 
-### `yarn build`
+Com o repositório baixado na sua máquina, navegue até a pasta do projeto clonado e execute o comando `npm install` para instalar todas as dependências.
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
+```shell
+cd fusion-practical-exam
+npm install
+```
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
+### `Rodando a Aplicação`
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+Agora é só executar a aplicação
 
-### `yarn eject`
+```shell
+npm run start
+```
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+> Isso executará a aplicação em modo de desenvolvimento
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+![Fusion DMS](/src/assets/screen-01.png)
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
--- src/App.js
+++ src/App.js
@@ -1,26 +1,169 @@
 import React from 'react';
-import logo from './logo.svg';
+import { DataGrid, GridColumn, Form, Dialog, TextBox, Label, LinkButton, ButtonGroup, ComboBox } from 'rc-easyui';
+
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const situations = [
+  { value: 1, text: 'Livre' }, 
+  { value: 2, text: 'Em Curso' }, 
+  { value: 3, text: 'Retomando' }
+];
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      pageSize: 10,
+      pageNumber: 1,
+      editingRow: null,
+      model: {}, 
+      rules: {
+        'name': 'required',
+        'cpf': 'required',
+      },
+      closed: true,
+    };
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData() {
+    let data = [];
+    for (let i = 1; i <= 100; i++) {
+      let situation = situations[Math.floor(Math.random() * (3 - 0) + 0)].value;
+      let status = Math.floor(Math.random() * (2 - 0) + 0);
+      data.push({ id: i, name: `Motorista ${i}`, cpf: Math.random(), email: `motorista${i}@email.com`, situation, status });
+    }
+    this.setState({ data });
+  }
+
+  getError(name) {
+    const { errors } = this.state;
+    if (!errors) {
+      return null;
+    }
+    return errors[name] && errors[name].length ? errors[name][0] : null;
+  }
+
+  handleChangePage(event) {
+    this.setState({ ...event });
+  }
+
+  handleEditRow(row) {
+    this.setState({ editingRow: row, model: Object.assign({}, row), title: 'Edit', closed: false });
+  }
+
+  handleCreateRow(row) {
+    this.setState({ editingRow: null, model: {}, title: 'Create', closed: false });
+  }
+
+  handleSaveRow() {
+    this.form.validate(() => {
+      if (this.form.valid()) {
+        let data = this.state.data.slice();
+        let row = Object.assign({}, this.state.editingRow, this.state.model);
+        row.id = data.length + 1;
+        if (this.state.editingRow) {
+          let index = data.indexOf(this.state.editingRow);
+          data.splice(index, 1, row);
+          
+        }
+        else {
+          data.push(row);
+        }
+        this.setState({ data, closed: true });
+      }
+    })
+  }
+
+  handleDeleteRow(row) {
+    const { data } = this.state;
+    this.setState({ data: data.filter(r => r !== row) });
+  }
+
+  handleCloseForm(event) {
+    this.setState({ closed: true });
+  }
+
+  renderDialog() {
+    const row = this.state.model;
+    const { title, closed, rules } = this.state;
+    return (
+      <Dialog modal title={title} className="dialogForm" closed={closed} onClose={(event) => this.handleCloseForm() }>
+        <div className="f-full">
+          <Form className="f-full" ref={ref => this.form = ref} model={row} rules={rules} onValidate={(errors) => this.setState({ errors: errors })}>
+            <div>
+              <Label htmlFor="name">Name:</Label>
+              <TextBox inputId="name" name="name" value={row.name}></TextBox>
+              <div className="error">{this.getError('name')}</div>
+            </div>
+            <div>
+              <Label htmlFor="cpf">CPF:</Label>
+              <TextBox inputId="cpf" name="cpf" value={row.cpf}></TextBox>
+              <div className="error">{this.getError('cpf')}</div>
+            </div>
+            <div>
+              <Label htmlFor="email">Email:</Label>
+              <TextBox inputId="email" name="email" value={row.email}></TextBox>
+              <div className="error">{this.getError('email')}</div>
+            </div>
+            <div>
+              <Label htmlFor="situation">Situação:</Label>
+              <ComboBox inputId="situation" name="situation" value={row.situation} data={situations} />
+              <div className="error">{this.getError('situation')}</div>
+            </div>
+            <div>
+              <Label htmlFor="status">Status:</Label>
+              <ComboBox inputId="status" name="status" value={row.status} data={[{ value: 0, text: 'Inativo' }, { value: 1, text: 'Ativo' }]} />
+              <div className="error">{this.getError('status')}</div>
+            </div>
+          </Form>
+        </div>
+        <div className="dialog-button">
+          <LinkButton onClick={() => this.handleSaveRow()}>Save</LinkButton>
+          <LinkButton onClick={() => this.handleCloseForm()}>Close</LinkButton>
+        </div>
+      </Dialog>
+    )
+  }
+
+  render() {
+    const { data, pageNumber, pageSize } = this.state;
+    return (
+      <>
+        <div className="container">
+          <h1>Fusion - Motoristas</h1>
+          <div>
+            <LinkButton onClick={() => this.handleCreateRow()} className="btnCreate">Cadastrar Motorista</LinkButton>
+          </div>
+          <DataGrid data={data} pagination clickToEdit selectionMode="cell" editMode="cell" total={data.length} pageSize={pageSize} pageNumber={pageNumber} onPageChange={event => this.handleChangePage(event)} className="dataGrid">
+            <GridColumn field="name" title="Nome"></GridColumn>
+            <GridColumn field="cpf" title="CPF"></GridColumn>
+            <GridColumn field="email" title="Email"></GridColumn>
+            <GridColumn field="situation" title="Situação" render={({ row }) => situations[row.situation-1].text }></GridColumn>
+            <GridColumn field="status" title="Status" render={({ row }) => ['Inativo', 'Ativo'][row.status]}></GridColumn>
+            <GridColumn field="act" title="Actions" align="center" width={110}
+              render={({ row }) => (
+                <>
+                  <ButtonGroup style={{ padding: 5 }}>
+                    <LinkButton onClick={() => this.handleEditRow(row)}>Edit</LinkButton>
+                    <LinkButton onClick={() => this.handleDeleteRow(row)}>Delete</LinkButton>
+                  </ButtonGroup>
+                </>
+              )}
+            />
+          </DataGrid>
+          <p>Total: {data.length} / Por Página: {pageSize} / Página Atual: {pageNumber}</p>
+        </div>
+        {this.renderDialog()}
+      </>
+    );
+  }
 }
 
 export default App;
